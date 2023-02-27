f314554df447f2d3d9a365d6bd5a0236940996d4
tirthankarCU <tirthankar.mittra@colorado.edu>
2023-02-27 10:13:36 -0700

    EnvVisionFinish

--- gym-examples/gym_examples/envs/rl_nlp_world.py
+++ gym-examples/gym_examples/envs/rl_nlp_world.py
@@ -2,62 +2,51 @@ import gym
 from gym import spaces
 import pygame
 import numpy as np
+from enum import Enum 
+import vision_pyGame as vga
+'''
+ACTIONS
+'''
+class ACTION(Enum):
+    PICK_BIG=1
+    PICK_MED=2
+    PICK_SMALL=3
+    PUT_BIG=4
+    PUT_MED=5
+    PUT_SMALL=6
 
+'''
+BOX TYPE
+'''
+class BOXTYPE(Enum):
+    NONE=0
+    BIG=1
+    MEDIUM=2
+    SMALL=3
 
 class RlNlpWorld(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
-
-    def __init__(self, render_mode=None, size=5):
-        self.size = size  # The size of the square grid
-        self.window_size = 512  # The size of the PyGame window
-
-        # Observations are dictionaries with the agent's and the target's location.
-        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
-        self.observation_space = spaces.Dict(
-            {
-                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-            }
-        )
-
-        # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
-        self.action_space = spaces.Discrete(4)
-
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        I.e. 0 corresponds to "right", 1 to "up" etc.
-        """
-        self._action_to_direction = {
-            0: np.array([1, 0]),
-            1: np.array([0, 1]),
-            2: np.array([-1, 0]),
-            3: np.array([0, -1]),
-        }
-
-        assert render_mode is None or render_mode in self.metadata["render_modes"]
-        self.render_mode = render_mode
-
-        """
-        If human-rendering is used, `self.window` will be a reference
-        to the window that we draw to. `self.clock` will be a clock that is used
-        to ensure that the environment is rendered at the correct framerate in
-        human-mode. They will remain `None` until human-mode is used for the
-        first time.
-        """
-        self.window = None
-        self.clock = None
-
+############################################
+    def __init__(self,mx_timeSteps=50,render_mode=None):
+        self.no=np.random.randint(0,1000)[0]
+        self.carry=False
+        self.boxType=BOXTYPE.NONE
+        self.mode=0
+        if render_mode=='rgb_array':
+            self.mode=1
+        self._visual=vga.draw_main(self.metadata['render_modes'][self.mode],self.metadata['render_fps'],self.no)
+        self._text='TBD'
+        self._question='TBD'
+        self.mx_timeSteps,self.curr_time=mx_timeSteps,0
+############################################
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
+        return {"text": self._text,"question":self._quesiton,"visual": self._visual}
 
     def _get_info(self):
         return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
-            )
+            "progress": "currently feature not required."
         }
-
+############################################
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
@@ -79,92 +68,70 @@ class RlNlpWorld(gym.Env):
             self._render_frame()
 
         return observation, info
-
+############################################
     def step(self, action):
-        # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
-        # We use `np.clip` to make sure we don't leave the grid
-        self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
-        )
-        # An episode is done iff the agent has reached the target
-        terminated = np.array_equal(self._agent_location, self._target_location)
-        reward = 1 if terminated else 0  # Binary sparse rewards
+
+        def pick(boxArr,b_type):
+            if self.carry==False:
+                for box in boxArr:
+                    if not box.isEmpty:
+                        self.carry=True 
+                        self.boxType=b_type
+                        box.isEmpty=False
+                        return 0 
+            else:
+                return -1
+            
+        def put(b_type):
+            if self.carry==False or self.boxType!=b_type:
+                return -1
+            self.boxType=BOXTYPE.NONE 
+            self.carry=False 
+            for box in vga.constructArrElement[b_type.value-1]:
+                if box.isEmpty:
+                    box.isEmpty=False 
+                    return 0 
+                
+        def checkSolution():
+            result,power=0,100
+            for c in vga.constructArrElement:
+                cnt_box=0
+                for box in c:
+                    if not box.isEmpty:
+                        cnt_box+=1
+                result+=cnt_box*power 
+                power/=10
+            return True if self.no==result else False 
+        
+        if action==ACTION.PICK_BIG:
+            reward=pick(vga.big_block,BOXTYPE.BIG)
+        elif action==ACTION.PICK_MED:
+            reward=pick(vga.medium_block,BOXTYPE.MEDIUM)
+        elif action==ACTION.PICK_SMALL:
+            reward=pick(vga.small_block,BOXTYPE.SMALL)
+        elif action==ACTION.PUT_BIG:
+            reward=put(BOXTYPE.BIG)
+        elif action==ACTION.PUT_MED:
+            reward=put(BOXTYPE.MEDIUM)
+        elif action==ACTION.PUT_SMALL:
+            reward=put(BOXTYPE.SMALL)
+
+        self._visual=vga.drawAgain()
+        self.curr_time+=1
+        terminated = True if self.curr_time>self.mx_timeSteps else checkSolution()
+        if terminated:
+            sign=1 if checkSolution() else -1
+        reward = sign*10 if terminated else reward
         observation = self._get_obs()
         info = self._get_info()
-
-        if self.render_mode == "human":
-            self._render_frame()
-
         return observation, reward, terminated, False, info
-
+############################################
     def render(self):
-        if self.render_mode == "rgb_array":
-            return self._render_frame()
-
+        pass 
+############################################
     def _render_frame(self):
-        if self.window is None and self.render_mode == "human":
-            pygame.init()
-            pygame.display.init()
-            self.window = pygame.display.set_mode((self.window_size, self.window_size))
-        if self.clock is None and self.render_mode == "human":
-            self.clock = pygame.time.Clock()
-
-        canvas = pygame.Surface((self.window_size, self.window_size))
-        canvas.fill((255, 255, 255))
-        pix_square_size = (
-            self.window_size / self.size
-        )  # The size of a single grid square in pixels
-
-        # First we draw the target
-        pygame.draw.rect(
-            canvas,
-            (255, 0, 0),
-            pygame.Rect(
-                pix_square_size * self._target_location,
-                (pix_square_size, pix_square_size),
-            ),
-        )
-        # Now we draw the agent
-        pygame.draw.circle(
-            canvas,
-            (0, 0, 255),
-            (self._agent_location + 0.5) * pix_square_size,
-            pix_square_size / 3,
-        )
-
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
-            pygame.draw.line(
-                canvas,
-                0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
-                width=3,
-            )
-            pygame.draw.line(
-                canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
-            )
-
-        if self.render_mode == "human":
-            # The following line copies our drawings from `canvas` to the visible window
-            self.window.blit(canvas, canvas.get_rect())
-            pygame.event.pump()
-            pygame.display.update()
-
-            # We need to ensure that human-rendering occurs at the predefined framerate.
-            # The following line will automatically add a delay to keep the framerate stable.
-            self.clock.tick(self.metadata["render_fps"])
-        else:  # rgb_array
-            return np.transpose(
-                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
-            )
-
+        pass
+############################################
     def close(self):
-        if self.window is not None:
-            pygame.display.quit()
-            pygame.quit()
+        pass
+############################################
