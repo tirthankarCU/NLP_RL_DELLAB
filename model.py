72dcb0f71fa7aa87150797fe2ee2c4478e175723
tirthankarCU <tirthankar.mittra@colorado.edu>
2023-04-17 19:21:50 -0600

    ZZ:

--- model.py
+++ model.py
@@ -1,12 +1,17 @@
 import torch 
 import torch.nn as nn 
 import torchvision.models as models 
+import torch.nn.functional as F
+import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt 
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 import seaborn 
+import utils as U
+import json
 
+noOfActions=6
 class NNModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -42,9 +47,9 @@ class NNModel(nn.Module):
         randDQNSz=100
         self.dqn=nn.Sequential(
             nn.Linear(actionSize+self.important_features_image,randDQNSz),
-            nn.Relu(),
+            nn.ReLU(),
             nn.Linear(randDQNSz,1),
-            nn.Relu()
+            nn.ReLU()
         )
     def forward(self,image,action):
         op=self.resnet(image)
@@ -61,24 +66,50 @@ class NNModel(nn.Module):
 '''
 def lossDqn(p,y):
     return torch.mean((p-y)**2,dim=1)
-def lossImage(s,ns):
-    pass
-def train(model,reward_pred,reward_true,STATE,NEXT_STATE,device,optim,epoch,verbose=False):
+def lossImage(img_yp,img_y):
+    return torch.mean((img_yp-img_y)**2)
+def train(model,reward_true,STATE,NEXT_STATE,ACTION,device,optim,epoch,verbose=False):
     model.train()
+    IMG_X,IMG_Y=np.array([_state_["visual"] for _state_ in STATE]),np.array([_state_["visual"] for _state_ in NEXT_STATE])
+    IMG_X,IMG_Y=IMG_X.to(device),IMG_Y.to(device) # it should be torch
+    ACTION=ACTION.to(device)
+    IMG_YP,Q=model(IMG_X,ACTION)
     optim.zero_grad()
-    loss_dqn=lossDqn(reward_pred,reward_true)
-    loss_image=lossImage(STATE,NEXT_STATE)
+    loss_dqn=lossDqn(Q,reward_true)
+    loss_image=lossImage(IMG_YP,IMG_Y)
     loss_dqn.backward()
     loss_image.backward()
     optim.step() 
-    for batch_idx,(X,Y1,Y2) in enumerate(train_loader):
-        if batch_idx % 100 == 0 and verbose:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * X.shape[0], len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-    if verbose:
-        print('---------------------------')
+    if epoch % 10 == 0 and verbose:
+        print(f'Train Epoch:{epoch} {loss_dqn} {loss_image}')
+
+def predict(model,STATE,device,verbose=False):
+    global noOfActions
+    model.eval()
+    with torch.no_grad():
+        IMG=np.array([_state_["visual"] for _state_ in STATE])
+        # CONVERT TO TENSOR #
+        IMG=torch.from_numpy(IMG)
+        # CONVERT TO TENSOR #
+        IMG=IMG.to(device)
+        for actions in range(noOfActions):
+            IMG_YP,Q=model(IMG,U.oneHot(noOfActions,actions))
+            QA.append(Q)
+        QA=np.array(QA)
+        QA_max=np.max(QA,axis=1)
+        return QA_max,np.argmax(QA,axis=1) 
 
 if __name__=='__main__':
-    obj=NNModel()
-    obj.display()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model=NNModel().to(device)
+    model_traget=NNModel().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.5)
+    f=open("my_dict.json")
+    data=json.load(f)
+    state=data
+    state["visual"]=np.array(state["visual"])
+    plt.imshow(state["visual"])
+    # print(type(state["visual"]))
+    # state["visual"]=U.phi(state["visual"])
+    # print(state["visual"].shape)
+    # action=(predict(model,[state],device))[1]
--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import cv2
-sns.set_theme()
 
 def plot(data,ylb,title):
     plt.plot([i for i in range(1,)],data,color='mediumvioletred',marker='o')
@@ -11,12 +10,17 @@ def plot(data,ylb,title):
     plt.ylabel(ylb)
     plt.savefig(f'results/{title}.png')
 
-def crop(arr,scale_percent=50):
+def crop_resize(arr):
     crop_arr=arr[5:-200,5:,:3] # 10 & 50 these numbers are found out after trial & error.
-    width = int(crop_arr.shape[1] * scale_percent / 100)
-    height = int(crop_arr.shape[0] * scale_percent / 100)
-    output = cv2.resize(crop_arr,(width, height),interpolation=cv2.INTER_CUBIC)
+    print(crop_arr.shape)
+    print(type(crop_arr))
+    width,height = 224,224
+    output = cv2.resize(crop_arr,(width, height),interpolation=cv2.INTER_CUBIC) # This is for ResNet-18.
     return output
+
+def phi(state_vis):
+    print(state_vis.shape)
+    return crop_resize(state_vis) 
 
 def oneHot(mx,x):
     return np.array([0 if i!=x else 1 for i in range(mx)])
